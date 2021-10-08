67dcaf1d3aae49830ea8679700c3430aa30e60db
iuliamoldovan21 <iulliaa266@gmail.com>
2021-10-08 13:24:10 +0300

    state used on button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,28 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React!');
+
+  const clickHandler = () => {
+    setTitle('Updated!'); //!= title='Updated'
+  };
+
+  //how we would listen to events without React; imperative way
+  //document.getElementById('root').addEventListener()
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
